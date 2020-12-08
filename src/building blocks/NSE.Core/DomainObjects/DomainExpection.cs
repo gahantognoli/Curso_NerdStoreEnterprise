c3897f95129411907f960fcee28b19a34951f407
Gabriel Antognoli <gabriel_antognoli@hotmail.com>
2020-12-07 23:04:59 -0300

    Modelagem da entidade de Cliente e Endereco.

---
+++ src/building blocks/NSE.Core/DomainObjects/DomainExpection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace NSE.Core.DomainObjects
+{
+    public class DomainExpection : Exception
+    {
+        public DomainExpection()
+        {
+        }
+
+        public DomainExpection(string message) : base(message)
+        {
+        }
+
+        public DomainExpection(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
